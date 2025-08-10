c7077c07c7edb53ab6609072659614e6258d47d5
erik0330 <erikkaryan0330@gmail.com>
2025-08-10 15:21:32 +0200

    make get_project_by_id public

--- backend/server/src/routes/public.rs
+++ backend/server/src/routes/public.rs
@@ -3,8 +3,8 @@ use crate::{
         bounty_handler::{get_bounties, get_bounty_by_id, get_similar_bounties},
         prediction_handler::{get_prediction_by_id, get_predictions, get_top_predictors},
         project_handler::{
-            get_daos, get_project_counts_by_status, get_project_ids, get_projects,
-            get_similar_projects,
+            get_daos, get_project_by_id, get_project_counts_by_status, get_project_ids,
+            get_projects, get_similar_projects,
         },
         user_handler::get_user_profile_by_username,
         util_handler::{get_categories, get_category_by_id},
@@ -19,6 +19,7 @@ pub fn routes() -> Router<AppState> {
         .route("/util/category", get(get_categories))
         .route("/util/category/:id", get(get_category_by_id))
         .route("/project", get(get_projects))
+        .route("/project/:id", get(get_project_by_id))
         .route("/project/counts", get(get_project_counts_by_status))
         .route("/project/ids", get(get_project_ids))
         .route("/project/:id/similar", get(get_similar_projects))
