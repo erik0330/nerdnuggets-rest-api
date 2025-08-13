e8fe497ed18e9fca2f0088554fba6810a171b7f9
erik0330 <erikkaryan0330@gmail.com>
2025-08-13 17:03:17 +0200

    set get_bids of bounty as pubic

--- backend/server/src/routes/public.rs
+++ backend/server/src/routes/public.rs
@@ -1,6 +1,6 @@
 use crate::{
     handler::{
-        bounty_handler::{get_bounties, get_bounty_by_id, get_similar_bounties},
+        bounty_handler::{get_bids, get_bounties, get_bounty_by_id, get_similar_bounties},
         prediction_handler::{get_prediction_by_id, get_predictions, get_top_predictors},
         project_handler::{
             get_daos, get_project_by_id, get_project_counts_by_status, get_project_ids,
@@ -26,6 +26,7 @@ pub fn routes() -> Router<AppState> {
         .route("/dao", get(get_daos))
         .route("/bounty", get(get_bounties))
         .route("/bounty/:id", get(get_bounty_by_id))
+        .route("/bounty/:id/bid", get(get_bids))
         .route("/bounty/:id/similar", get(get_similar_bounties))
         .route("/prediction", get(get_predictions))
         .route("/prediction/:id", get(get_prediction_by_id))
