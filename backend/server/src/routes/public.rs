828de7f9b3ab3bf0ba0d08ce5369d1427cff8098
erik0330 <erikkaryan0330@gmail.com>
2025-08-14 09:02:34 +0200

    make get_institutions public

--- backend/server/src/routes/public.rs
+++ backend/server/src/routes/public.rs
@@ -7,7 +7,7 @@ use crate::{
             get_projects, get_similar_projects,
         },
         user_handler::get_user_profile_by_username,
-        util_handler::{get_categories, get_category_by_id},
+        util_handler::{get_categories, get_category_by_id, get_institutions},
     },
     state::AppState,
 };
@@ -18,6 +18,7 @@ pub fn routes() -> Router<AppState> {
         .route("/user/profile/:username", get(get_user_profile_by_username))
         .route("/util/category", get(get_categories))
         .route("/util/category/:id", get(get_category_by_id))
+        .route("/util/institutions", get(get_institutions))
         .route("/project", get(get_projects))
         .route("/project/:id", get(get_project_by_id))
         .route("/project/counts", get(get_project_counts_by_status))
--- backend/server/src/routes/util.rs
+++ backend/server/src/routes/util.rs
@@ -1,7 +1,7 @@
 use crate::{
     handler::util_handler::{
         create_category, extract_project_info, get_city, get_country, get_institution_detail,
-        get_institutions, remove_file_from_s3, upload_file_to_s3,
+        remove_file_from_s3, upload_file_to_s3,
     },
     state::AppState,
 };
@@ -14,7 +14,6 @@ pub fn routes() -> Router<AppState> {
     Router::new()
         .route("/util/country", get(get_country))
         .route("/util/city", get(get_city))
-        .route("/util/institutions", get(get_institutions))
         .route("/util/institution/detail", get(get_institution_detail))
         .route("/util/category", post(create_category))
         .route("/util/file/:folder", post(upload_file_to_s3))
