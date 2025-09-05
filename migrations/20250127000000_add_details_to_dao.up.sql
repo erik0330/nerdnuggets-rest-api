3c140293ca67dae79c78299578293d2c1159bc83
erik0330 <erikkaryan0330@gmail.com>
2025-09-05 09:15:29 +0200

    Add details column to dao table and populate from project data

--- migrations/20250127000000_add_details_to_dao.up.sql
+++ migrations/20250127000000_add_details_to_dao.up.sql
@@ -1,3 +1,10 @@
 -- Add up migration script here
 
+-- Add the details column to dao table
 ALTER TABLE dao ADD COLUMN IF NOT EXISTS details TEXT;
+
+-- Copy details from project to existing daos
+UPDATE dao 
+SET details = p.details 
+FROM project p 
+WHERE dao.project_id = p.id;
